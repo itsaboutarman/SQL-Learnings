1ef1a411dca186defc2831527428334c6fc6f158
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-13 10:21:46 +0330

    Joining Across Databases

--- queries.sql
+++ queries.sql
@@ -1,12 +1,8 @@
 USE sql_store;
 
-SELECT order_id, o.customer_id, first_name, last_name
---notice that when we have a same column in both tables, we need to specify
---which table we are referring to -> orders.customer_id
-
-FROM orders o
--- o is an alias for orders
-
-JOIN customers c --or we can use INNER JOIN
--- c is an alias for customers
-    ON o.customer_id = c.customer_id
+SELECT * 
+FROM order_items oi
+JOIN sql_inventory.products p
+--not that you have to prefix the tables
+-- that are not part of the current database 
+    ON oi.product_id = p.product_id;
