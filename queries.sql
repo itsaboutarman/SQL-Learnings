1a74c3c36ce7f35ab3687389724fa595ef033228
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-13 14:58:57 +0330

    Unions

--- queries.sql
+++ queries.sql
@@ -1,24 +1,45 @@
--- we use Cross Join to combine every record from the first table
--- with every record from the second table.
+USE sql_store;
+
+SELECT 
+    order_id,
+    order_date,
+    'Active' AS status
+FROM orders o
+WHERE order_date >= '2019-01-01'
+UNION
+SELECT 
+    order_id,
+    order_date,
+    'Archived' AS status
+FROM orders o
+WHERE order_date < '2019-01-01'
+
+-------------------------------------------------------------------
+-- Another example:
 
 USE sql_store;
 
 SELECT 
-    c.first_name AS customet,
-    p.name AS product
-
-FROM customers c
-
-CROSS JOIN products p
-
-ORDER BY c.first_name
-
--- Another way to write this:
+	customer_id, 
+    first_name,
+    points,
+    'Gold' AS type
+FROM customers
+WHERE points >= 3000
+UNION 
 SELECT 
-    c.first_name AS customet,
-    p.name AS product
-
-FROM customers c , products p
-
-ORDER BY c.first_name
+	customer_id, 
+    first_name,
+    points,
+    'Silver'
+FROM customers
+WHERE points between 2000 and 3000
+UNION
+SELECT 
+	customer_id, 
+    first_name,
+    points,
+    'Bronze'
+FROM customers
+WHERE points < 2000
 
