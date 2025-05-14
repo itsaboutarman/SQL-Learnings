4d22fc209908a68492664e075880b7d8e79b6ecc
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-14 14:34:59 +0330

    Updating a Single Row

--- queries.sql
+++ queries.sql
@@ -1,13 +1,5 @@
 USE sql_store;
 
-CREATE TABLE orders_archived AS 
-SELECT * FROM orders
- -- notice that when you copy the data from one table to another,
- -- the table settings, primary keys and default values are not copied. 
-
-----------------------------------------------------------------
--- you can also copy a range of rows
-INSERT INTO orders_archived
-SELECT *
-FROM orders
-WHERE order_date < '2025-01-01'
+UPDATE invoices 
+SET payment_total = 10, payment_date = '2025-06-06'
+WHERE invoice_id = 1
