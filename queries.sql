23689d5df82694f478990e5939a93700b1abe867
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-14 13:43:30 +0330

    Inserting Multiple Rows

--- queries.sql
+++ queries.sql
@@ -1,27 +1,6 @@
 USE sql_store;
 
-INSERT INTO customers
-VALUES(
-    DEFAULT,
-    'Arman',
-    'Salehi',
-    '2003-09-21',
-    NULL,
-    'Address',
-    'City',
-    DEFAULT
-)
--------------------------------------------------------------
--- we can suply the list of columns 
--- that we want to insert values into
-INSERT INTO customers( 
-    first_name,
-    last_name,
-    birth_date
-    )
-
-VALUES(
-    'Arman',
-    'Salehi',
-    '2003-09-21',
-)
+INSERT INTO shippers(name)
+VALUES('shipper1'),
+      ('shipper2'),
+      ('shipper3')
