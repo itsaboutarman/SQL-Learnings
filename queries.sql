270b3082bae7e08f5dcfbafa34408f554e814cd9
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-15 00:06:00 +0330

    Deleting Rows

--- queries.sql
+++ queries.sql
@@ -1,12 +1,7 @@
 USE sql_store;
 
-UPDATE invoices
-SET 
-    payment_total = invoice_total * 0.5,
-    payment_date = due_date
-WHERE client_id IN 
-                (SELECT client_id
-                FROM clients
-                WHERE state IN ('CA', 'NY'));
-
--- we pass a subquery to the WHERE clause
+DELETE FROM invoices -- if we just execute this query, it will delete all the rows from the table
+WHERE client_id = 
+            (SELECT * 
+            FROM clients
+            WHERE name = 'Myworks');
