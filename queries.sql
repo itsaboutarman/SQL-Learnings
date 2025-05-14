0b17c15d97b7dc71410cfc0b70ec0e7b1cc12b37
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-14 13:39:34 +0330

    Inserting a row

--- queries.sql
+++ queries.sql
@@ -1,45 +1,27 @@
 USE sql_store;
 
-SELECT 
-    order_id,
-    order_date,
-    'Active' AS status
-FROM orders o
-WHERE order_date >= '2019-01-01'
-UNION
-SELECT 
-    order_id,
-    order_date,
-    'Archived' AS status
-FROM orders o
-WHERE order_date < '2019-01-01'
-
--------------------------------------------------------------------
--- Another example:
-
-USE sql_store;
-
-SELECT 
-	customer_id, 
+INSERT INTO customers
+VALUES(
+    DEFAULT,
+    'Arman',
+    'Salehi',
+    '2003-09-21',
+    NULL,
+    'Address',
+    'City',
+    DEFAULT
+)
+-------------------------------------------------------------
+-- we can suply the list of columns 
+-- that we want to insert values into
+INSERT INTO customers( 
     first_name,
-    points,
-    'Gold' AS type
-FROM customers
-WHERE points >= 3000
-UNION 
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    'Silver'
-FROM customers
-WHERE points between 2000 and 3000
-UNION
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    'Bronze'
-FROM customers
-WHERE points < 2000
+    last_name,
+    birth_date
+    )
 
+VALUES(
+    'Arman',
+    'Salehi',
+    '2003-09-21',
+)
