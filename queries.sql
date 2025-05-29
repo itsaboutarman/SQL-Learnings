6c93e84f1c7794942d0eb69fc9a14e67783b17bc
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-29 20:14:56 +0330

    Date Functions in MySQL

--- queries.sql
+++ queries.sql
@@ -1,16 +1,4 @@
-SELECT LENGTH ('blue') --> 4
-SELECT UPPER ('blue') --> BLUE
-SELECT LOWER ('Blue') --> blue
-SELECT LTRIM ('     blue') --> blue
-SELECT LTRIM ('blue     ') --> blue
-SELECT TRIM ('   blue     ') --> blue
-SELECT LEFT ('Arman Salehi' , 5) --> Arman
-SELECT RIGHT ('Arman Salehi' , 6) --> Salehi
-SELECT SUBSTRING ('Arman Salehi' , 3, 6) --> man Sa
-SELECT SUBSTRING ('Arman Salehi' , 3) --> man Salehi
-SELECT SUBSTRING ('Arman Salehi' , 3) --> man Salehi
-SELECT SUBSTRING ('s', 'Arman Salehi') --> 7 (search is not case sensitive)
-SELECT SUBSTRING ('z', 'Arman Salehi') --> 0 (when the character is not found, it returns 0)
-SELECT REPLACE ('Arman Salehi', 'Arman', 'Mahan') --> Mahan Salehi
-SELECT CONCAT ('Arman', ' ', 'Mahan') --> Arman Mahan
-
+SELECT NOW(), CURDATE(), CURTIME();
+SELECT YEAR(NOW()), MONTH(NOW()), DAY(NOW()), HOUR(NOW()), SECOND(NOW())
+SELECT DAYNAMEE(NOW()), MONTHNAME(NOW())
+SELECT EXTRACT(DAY FROM NOW()) -- this query is a part of standard SQL language
