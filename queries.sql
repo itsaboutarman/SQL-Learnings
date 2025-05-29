957d7ac1138bce202691a674b976f744ea35d585
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-29 21:09:31 +0330

    The CASE Operator in MySQL

--- queries.sql
+++ queries.sql
@@ -1,27 +1,41 @@
 USE sql_store;
 
 SELECT 
-    order_id,
-    order_date,
-    IF(YEAR(order_date) = YEAR(NOW()), 'Active', 'Archived') AS status
-FROM orders 
+	customer_id, 
+    first_name,
+    points,
+    CASE
+        WHEN points >= 3000 THEN 'Gold'
+        WHEN points between 2000 AND 3000 THEN 'Silver'
+        WHEN points < 2000 THEN 'Bronze'
+        ELSE 'Future'
+	END AS category
+FROM customers
 
---equal to:
+
+-- equal to:
 SELECT 
-    order_id,
-    order_date,
-    'Active' AS status
-FROM orders o
-WHERE YEAR(order_date) = YEAR(NOW())
+	customer_id, 
+    first_name,
+    points,
+    'Gold' AS category
+FROM customers
+WHERE points >= 3000
+
+UNION 
+SELECT 
+	customer_id, 
+    first_name,
+    points,
+    'Silver' AS category
+FROM customers
+WHERE points between 2000 and 3000
 
 UNION
 SELECT 
-    order_id,
-    order_date,
-    'Archived' AS status
-FROM orders o
-WHERE NOT (YEAR(order_date) = YEAR(NOW()))
-
-
-
-
+	customer_id, 
+    first_name,
+    points,
+    'Bronze' AS category
+FROM customers
+WHERE points < 2000
