7e68bb097781e3747e4fcf2443f44ba442e61ab6
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-29 20:40:19 +0330

    The IFNULL and COALESCE Functions in MySQL

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
-SELECT NOW(), CURDATE(), CURTIME();
-SELECT YEAR(NOW()), MONTH(NOW()), DAY(NOW()), HOUR(NOW()), SECOND(NOW())
-SELECT DAYNAMEE(NOW()), MONTHNAME(NOW())
-SELECT EXTRACT(DAY FROM NOW()) -- this query is a part of standard SQL language
+USE sql_store;
+
+SELECT 
+    order_id,
+    IFNULL(shipper_id, 'Not Assigned') AS shipper -- if shipper_id is null, then assign 'Not Assigned'
+    COALESCE(shipper_id, comments, 'Not Assigned') AS shipper_id
+
+FROM oreders
+
+-- with the IFNULL function, we can substitute a value for null values
+-- with the COALESCE function, we supply a list of values, and it will return the first non-null value
