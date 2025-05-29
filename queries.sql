8dd5e1b31c412149196e071a1befb658ad812dfc
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-29 13:12:35 +0330

    The ROLLUP Operator

--- queries.sql
+++ queries.sql
@@ -1,36 +1,14 @@
 USE sql_invoicing;
 
 SELECT 
-	client_id,
+	state, 
+    city, 
     SUM(invoice_total) AS total_sales
-    COUNT(*) AS number_of_invoices,
+FROM invoices
+JOIN clients c USING (client_id)
+group by state , city WITH ROLLUP
 
-FROM invoices i
+-- WITH ROLLUP creates additional summary rows that show aggregated values
+-- at different levels of grouping.
 
-GROUP BY client_id
-HAVING total_sales > 500 
-
--- we use WHERE clause to filter rows before grouping
-
--- we use HAVING clause to filter rows after grouping
--- note: the columns that we use in HAVING clause have to be part of our SELECT clause
-
--- in this example we cannot filter total_sales before grouping
-
-------------------------------------------------------------------------------------------------------------------
--- another example:
-
-USE sql_store;
-
-SELECT
-	c.customer_id,
-    c.first_name,
-    c.last_name,
-	SUM(oi.quantity * oi.unit_price) AS total_sales 
-		
-FROM customers c
-JOIN orders o USING (customer_id)
-JOIN order_items oi USING (order_id)
-WHERE state = 'VA'
-GROUP BY c.customer_id
-HAVING total > 100
+-- this query is only available in MySQL. 
