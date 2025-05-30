e6aecae5b96dac6fb08c94d59df5689a6811c0bd
itsaboutarman <armansalehi82@aut.ac.ir>
2025-05-30 13:10:36 +0330

    Creating Views

--- queries.sql
+++ queries.sql
@@ -1,41 +1,11 @@
-USE sql_store;
+USE sql_invoicing;
 
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    CASE
-        WHEN points >= 3000 THEN 'Gold'
-        WHEN points between 2000 AND 3000 THEN 'Silver'
-        WHEN points < 2000 THEN 'Bronze'
-        ELSE 'Future'
-	END AS category
-FROM customers
+CREATE VIEW clients_balance AS
+	SELECT 
+		c.client_id,
+		c.name,
+		SUM(i.invoice_total - i.payment_total) AS balance
+	FROM clients c
+	JOIN invoices i USING (client_id)
+    GROUP BY c.client_id, c.name
 
-
--- equal to:
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    'Gold' AS category
-FROM customers
-WHERE points >= 3000
-
-UNION 
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    'Silver' AS category
-FROM customers
-WHERE points between 2000 and 3000
-
-UNION
-SELECT 
-	customer_id, 
-    first_name,
-    points,
-    'Bronze' AS category
-FROM customers
-WHERE points < 2000
