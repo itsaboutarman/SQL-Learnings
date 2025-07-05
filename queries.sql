025159a7c710fe94ce630c04831b096c5661821f
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-05 14:14:36 +0330

    Triggers

--- queries.sql
+++ queries.sql
@@ -1,34 +1,14 @@
--- Functions return a value and can be used in SQL expressions. 
--- Procedures perform actions and can modify data but don't return values directly.
--- Functions return one value; procedures can return multiple values and perform operations.
-
-DROP FUNCTION IF EXISTS get_risk_factor_for_client;
+-- A trigger is an automatic action executed when specific database events occur.
 
 DELIMITER $$
-CREATE FUNCTION get_risk_factor_for_client(
-	client_id INT
-)
-RETURNS int
--- Function attributes like DETERMINISTIC, READS SQL DATA, etc.
--- define behavior, data access, and output consistency.
-
--- DETERMINISTIC means the function always returns the same result for the same input.
--- READS SQL DATA means the function only reads from the database (no modification).
-
-READS SQL DATA
+CREATE TRIGGER payments_after_insert
+	AFTER INSERT ON payments
+    FOR EACH ROW
 BEGIN
-	DECLARE risk_factor DECIMAL(9,2) DEFAULT 0;
-    DECLARE invoices_total DECIMAL(9,2);
-    DECLARE invoices_count INT;
-    
-    SELECT COUNT(*), SUM(invoice_total)
-    INTO invoices_count, invoices_total
-    FROM invoices i
-    WHERE i.client_id = client_id;
-    
-    SET risk_factor = invoices_total / invoices_count * 5;
-    
-RETURN risk_factor;
+	UPDATE invoices
+    SET payment_total = payment_total + NEW.amount
+    WHERE invoice_id = NEW.invoice_id;
 END $$
-
 DELIMITER ;
+
+-- Avoid modifying the same table inside its own trigger to prevent infinite recursion.
