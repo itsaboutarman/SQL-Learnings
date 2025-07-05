0818619b1fa207664756e4bea1de50407f02d7ee
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-05 20:35:12 +0330

    Deadlock

--- queries.sql
+++ queries.sql
@@ -1,15 +1,6 @@
-SHOW VARIABLES LIKE 'transaction_isolation';
-
-SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; --> You can choose any other isolation level
--- This sets the isolation level only for the current transaction.
--- You must run this *before* BEGIN / START TRANSACTION.
--- Affects only one transaction.
-
-SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
--- Sets the isolation level for all transactions in the current session (e.g., current connection).
--- Remains in effect until the session ends or another session-level setting overrides it.
-
-SET GLOBAL TRANSACTION ISOLATION LEVEL SERIALIZABLE;
--- Changes the default isolation level for *all* new sessions.
--- Requires administrative privileges.
--- Does NOT affect already open sessions.
+/*
+Deadlock occurs when two or more transactions each hold a lock on one resource and attempt to acquire a lock on the other’s resource, creating a circular wait.
+As a result, none of the transactions can proceed, and the system must intervene by rolling back one of them to break the cycle.
+Deadlocks are common in systems with concurrent transactions accessing the same data in different orders.
+To prevent deadlocks: keep transactions short, access resources in a consistent order, avoid user interaction inside transactions, and consider lower isolation levels when appropriate.
+*/
