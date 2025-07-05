b79d38dd36f98a4123bee16209801d9c9e68f860
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-05 15:03:53 +0330

    Viewing and Dropping Triggers

--- queries.sql
+++ queries.sql
@@ -1,14 +1,3 @@
--- A trigger is an automatic action executed when specific database events occur.
+SHOW TRIGGERS LIKE 'payments%'; -- Viewing
 
-DELIMITER $$
-CREATE TRIGGER payments_after_insert
-	AFTER INSERT ON payments
-    FOR EACH ROW
-BEGIN
-	UPDATE invoices
-    SET payment_total = payment_total + NEW.amount
-    WHERE invoice_id = NEW.invoice_id;
-END $$
-DELIMITER ;
-
--- Avoid modifying the same table inside its own trigger to prevent infinite recursion.
+DROP TRIGGER IF EXISTS payments_after_insert; -- Dropping
