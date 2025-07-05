887dcd33b4b1c947c91bfa2ffd1a4bb859b39473
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-05 09:27:42 +0330

    Parameters with Default Value

--- queries.sql
+++ queries.sql
@@ -3,11 +3,31 @@ DROP PROCEDURE IF EXISTS get_invoices_by_client;
 DELIMITER $$
 CREATE PROCEDURE get_invoices_by_client
 (
-	client_id INT(11)
+	client_id INT
 )
 BEGIN
+-- Example:	
+	IF client_id IS NULL
+		SET client_id = 2;
+	END IF;
 	SELECT *
 	FROM INVOICES i
 	WHERE i.client_id = client_id;
+
+-- Example:
+	IF client_id IS NULL
+		SELECT * FROM INVOICES i;
+	ELSE
+	SELECT *
+	FROM INVOICES i
+	WHERE i.client_id = client_id;
+	END IF;
+-- Another way to write this code:
+	SELECT *
+	FROM INVOICES i
+	WHERE i.client_id = IFNULL(client_id, i.client_id);
+	-- IFNULL(X, Y) returns X if it's not NULL; otherwise, it returns Y
+	
+
 END &&
 DELIMITER ;
