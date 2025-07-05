6f9196c23b55919add744f507e2c6d282c09e6f9
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-05 22:24:39 +0330

    Data Types

--- queries.sql
+++ queries.sql
@@ -1,6 +1,50 @@
+String Types:
+
+- CHAR(X)    fixed-length
+- VARCHAR(X) max: 65,535 character
+- MEDIUMTEXT max: 16 MB
+- LONGTEXT 	 max: 4 GB
+- TINYTEXT	 max: 255 bytes	
+- TEXT		 max: 64 KB
+---------------------------------------------------------------
+Integer Types:
+
+- TINYINY
+- UNSIGNED TINYINT
+- SMALLINT
+- MEDIUMINT
+- INT
+- BIGINT
+---------------------------------------------------------------
+Fixed-point and Floating-point Types:
+
+- DECIMAL(p, s)
+- FLOAT
+- DOUBLE
+---------------------------------------------------------------
+Boolean Types:
+
+- True 1
+- False 0
+---------------------------------------------------------------
+Date and Time Types:
+
+- DATE 
+- TIME 
+- DATETIME 
+- TIMESTAMP
+- YEAR
+---------------------------------------------------------------
+Blob Types:
+-- we use blob types to store large amounts of binary data like images
+- TINYBLOB		255b
+- BLOB 			65KB
+- MEDIUMBLOB	16MB
+- LONGBLOB		4GB
 /*
-Deadlock occurs when two or more transactions each hold a lock on one resource and attempt to acquire a lock on the other’s resource, creating a circular wait.
-As a result, none of the transactions can proceed, and the system must intervene by rolling back one of them to break the cycle.
-Deadlocks are common in systems with concurrent transactions accessing the same data in different orders.
-To prevent deadlocks: keep transactions short, access resources in a consistent order, avoid user interaction inside transactions, and consider lower isolation levels when appropriate.
+	Problems with storing files in a database:
+		1- increased database size
+		2- slower backups
+		3- performance problems
+		4- more code to read/write images
 */
