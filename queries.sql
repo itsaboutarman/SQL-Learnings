8c336d071d93b17768e7dc153b898032239aefcb
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-13 19:50:27 +0330

    Creating and Dropping Tables

--- queries.sql
+++ queries.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE IF NOT EXISTS sql_store2;
-DROP DATABASE IF EXISTS sql_store2;
+CREATE TABLE customers 
+(
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name  VARCHAR(50) NOT NULL,
+  points      INT NOT NULL DEFAULT 0,
+  email       VARCHAR(255) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS customers;
