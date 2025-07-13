03e0edf1ae6aafaa4ad545db81859d9694f56fb4
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-13 19:56:27 +0330

    Altering Tables

--- queries.sql
+++ queries.sql
@@ -1,9 +1,6 @@
-CREATE TABLE customers 
-(
-  customer_id INT PRIMARY KEY AUTO_INCREMENT,
-  first_name  VARCHAR(50) NOT NULL,
-  points      INT NOT NULL DEFAULT 0,
-  email       VARCHAR(255) NOT NULL UNIQUE
-);
-
-DROP TABLE IF EXISTS customers;
+ALTER TABLE customers
+  ADD last_name       VARCHAR(50) NOT NULL AFTER first_name,
+  ADD city            VARCHAR(50) NOT NULL,
+  MODIFY first_name   VARCHAR(55) DEFAULT '',
+  DROP points
+;
