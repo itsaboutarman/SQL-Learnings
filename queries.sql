34b96068ee2c385a8862467af43f426b97af43b6
itsaboutarman <armansalehi82@aut.ac.ir>
2025-07-15 17:12:02 +0330

    Creating Relationships

--- queries.sql
+++ queries.sql
@@ -1,6 +1,20 @@
-ALTER TABLE customers
-  ADD last_name       VARCHAR(50) NOT NULL AFTER first_name,
-  ADD city            VARCHAR(50) NOT NULL,
-  MODIFY first_name   VARCHAR(55) DEFAULT '',
-  DROP points
-;
+CREATE DATABASE IF NOT EXISTS sql_store2;
+USE sql_store2;
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers
+(
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name  VARCHAR(50) NOT NULL,
+  points      INT NOT NULL DEFAULT 0,
+  email       VARCHAR(255) NOT NULL UNIQUE
+);
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders
+(
+  order_id      INT PRIMARY KEY,
+  customer_id   INT NOT NULL,
+  FOREIGN KEY fk_orders_customers(customer_id)
+    REFERENCES customers (customer_id)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION
+);
